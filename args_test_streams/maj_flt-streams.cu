1ec5cca98693eada61fed60a9b40571cc1c037c1
xpeedster <59727579+xpeedster@users.noreply.github.com>
2022-01-14 10:57:13 +0100

    Update maj_flt-streams.cu
    mlockall works when run:  sudo docker run -it --cap-add IPC_LOCK --runtime=nvidia --entrypoint=/bin/bash main_image

--- args_test_streams/maj_flt-streams.cu
+++ args_test_streams/maj_flt-streams.cu
@@ -149,21 +149,21 @@ int main(int argc, char** argv)
 	pf_before = exec("ps -C maj_flt-streams -o maj_flt");
 
 	// CPU Affinity
-	//cpu_set_t mask;
-	//CPU_ZERO(&mask);
-	//CPU_SET(1, &mask);
-	//if(sched_setaffinity(0, sizeof(cpu_set_t), &mask) <0)
-	//{
-	//	perror("sched_setaffinity_failed");
-	//	exit(-1);
-	//}
+	cpu_set_t mask;
+	CPU_ZERO(&mask);
+	CPU_SET(1, &mask);
+	if(sched_setaffinity(0, sizeof(cpu_set_t), &mask) <0)
+	{
+		perror("sched_setaffinity_failed");
+		exit(-1);
+	}
 
 	// Memory locking
-	//if(mlockall(MCL_CURRENT | MCL_FUTURE) < 0)
-	//{
-	//	perror("mklockall failed");
-	//	exit(-1);
-	//}
+	if(mlockall(MCL_CURRENT | MCL_FUTURE) < 0)
+	{
+		perror("mklockall failed");
+		exit(-1);
+	}
 
 
 	cudaStream_t stream1;
